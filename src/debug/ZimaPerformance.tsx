c2a5ba5bf021e512347295bc1bf883c30c1c27f8
Telefragged <Vetle.braend@gmail.com>
2019-12-08 16:34:28 +0100

    Show current fps

--- src/debug/ZimaPerformance.tsx
+++ src/debug/ZimaPerformance.tsx
@@ -23,21 +23,9 @@ const ZimaPerformance : React.FunctionComponent = () => {
             const deltaT = now - prevTime.current;
 
             prevTime.current = now;
-
-            const times = frameTimes.current;
-            times.push(deltaT);
-
-            if(times.length > 250)
-                times.splice(0, 1);
-
-            let sum = 0;
-            for (let i = 0; i < times.length; i++)
-                sum += times[i];
-
-            const avg = (sum / 1000) / times.length;
             const font = ctx.font;
             ctx.font = '20px Verdana';
-            ctx.fillText('fps: ' + (1 / avg).toString(), 10, 30);
+            ctx.fillText('fps: ' + (1000 / deltaT).toString(), 10, 30);
             ctx.fillText('ms: ' + deltaT.toString(), 10, 60);
             ctx.font = font;
         }}
